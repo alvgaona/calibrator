6b2378bffc527311957bea9891aae5d6a17c9a4f
Alvaro <alvgaona@gmail.com>
2024-09-08 15:36:12 -0300

    feat: upload input file to bucket

--- src/components/CalibrateForm.tsx
+++ src/components/CalibrateForm.tsx
@@ -1,5 +1,6 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
+import { toast } from 'sonner';
 import { z } from 'zod';
 
 import { Button } from '@/components/ui/button';
@@ -13,6 +14,7 @@ import {
   FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import { v4 as uuidv4 } from 'uuid';
 
 const formSchema = z.object({
   dataset: z.string({ required_error: 'A name is required for the dataset' }),
@@ -22,17 +24,68 @@ const formSchema = z.object({
     }
 
     const file = value[0];
-    return file.type === 'application/x-gzip';
+    return (
+      file.type === 'application/x-gzip' || file.type === 'application/gzip'
+    );
   }, 'Please upload a tar.gz file'),
 });
 
+type FormValues = z.infer<typeof formSchema>;
+
 const CalibrateForm = () => {
-  const form = useForm<z.infer<typeof formSchema>>({
+  const form = useForm<FormValues>({
     resolver: zodResolver(formSchema),
   });
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values);
+  async function getPresignedUrl(
+    runId: string,
+    dataset: string,
+    fileName: string,
+  ): Promise<string> {
+    const response = await fetch(import.meta.env.PUBLIC_UPLOAD_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        runId,
+        dataset,
+        fileName,
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data.presignedUrl;
+  }
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    const file = (values.file as FileList)[0];
+
+    const presignedUrl = await getPresignedUrl(
+      uuidv4(),
+      values.dataset,
+      file.name,
+    );
+
+    const uploadResponse = await fetch(presignedUrl, {
+      method: 'PUT',
+      body: file,
+      headers: {
+        'Content-Type': 'application/gzip',
+      },
+    });
+
+    if (!uploadResponse.ok) {
+      toast.error(
+        `Upload failed. Please retry in a couple minutes or contact support.`,
+      );
+    }
+
+    toast.success(`File ${file.name} was uploaded succesfully`);
   }
 
   return (
@@ -45,7 +98,7 @@ const CalibrateForm = () => {
             <FormItem>
               <FormLabel>Dataset name</FormLabel>
               <FormControl>
-                <Input placeholder="steepest-descent" {...field} />
+                <Input placeholder="steepest descent" {...field} />
               </FormControl>
               <FormDescription>
                 This name will be used to create your input dataset
